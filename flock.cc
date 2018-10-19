22d7d4c55639d9d5550d9d5ef13f32cab19217da
Lucas Woo <legendecas@gmail.com>
2018-10-19 16:30:43 +0800

    Basic JS API implementation

--- flock.cc
+++ flock.cc
@@ -1,5 +1,7 @@
 #include <stdlib.h>
+#include <string.h>
 #include <sys/file.h>
+#include <sys/errno.h>
 #include <node_api.h>
 #include "common.h"
 
@@ -26,8 +28,13 @@ void FlockComplete (napi_env env, napi_status status, void* data) {
   napi_value callback;
   NAPI_CALL_RETURN_VOID(env, napi_get_reference_value(env, execute->callback_ref, &callback));
 
+  int ret = execute->ret;
+  if (ret != 0) {
+    ret = errno;
+  }
+
   napi_value nval_ret;
-  NAPI_CALL_RETURN_VOID(env, napi_create_int32(env, execute->ret, &nval_ret));
+  NAPI_CALL_RETURN_VOID(env, napi_create_int32(env, ret, &nval_ret));
 
   napi_value recv;
   NAPI_CALL_RETURN_VOID(env, napi_create_object(env, &recv));
@@ -44,33 +51,33 @@ void FlockComplete (napi_env env, napi_status status, void* data) {
 
 static napi_value Flock (napi_env env, napi_callback_info cb_info) {
   size_t argc = 3;
-  napi_value args[argc];
-  NAPI_CALL(env, napi_get_cb_info(env, cb_info, &argc, args, nullptr, nullptr));
+  napi_value argv[argc];
+  NAPI_CALL(env, napi_get_cb_info(env, cb_info, &argc, argv, nullptr, nullptr));
 
   napi_valuetype valuetype;
-  NAPI_CALL(env, napi_typeof(env, args[0], &valuetype));
+  NAPI_CALL(env, napi_typeof(env, argv[0], &valuetype));
   if (valuetype != napi_number) {
     NAPI_CALL(env, napi_throw_type_error(env, "", "Expect a number to be first argument of flock"));
   }
 
-  NAPI_CALL(env, napi_typeof(env, args[0], &valuetype));
+  NAPI_CALL(env, napi_typeof(env, argv[0], &valuetype));
   if (valuetype != napi_number) {
     NAPI_CALL(env, napi_throw_type_error(env, "", "Expect a number to be second argument of flock"));
   }
 
-  NAPI_CALL(env, napi_typeof(env, args[2], &valuetype));
+  NAPI_CALL(env, napi_typeof(env, argv[2], &valuetype));
   if (valuetype != napi_function) {
     NAPI_CALL(env, napi_throw_type_error(env, "", "Expect a function to be third argument of flock"));
   }
 
   int fd;
-  NAPI_CALL(env, napi_get_value_int32(env, args[0], &fd));
+  NAPI_CALL(env, napi_get_value_int32(env, argv[0], &fd));
 
   int operation;
-  NAPI_CALL(env, napi_get_value_int32(env, args[1], &operation));
+  NAPI_CALL(env, napi_get_value_int32(env, argv[1], &operation));
 
   napi_ref callback_ref;
-  NAPI_CALL(env, napi_create_reference(env, args[2], 1, &callback_ref));
+  NAPI_CALL(env, napi_create_reference(env, argv[2], 1, &callback_ref));
 
   flock_execute_data_t* execute = (flock_execute_data_t*) malloc(sizeof(flock_execute_data_t));
   execute->fd = fd;
@@ -79,8 +86,8 @@ static napi_value Flock (napi_env env, napi_callback_info cb_info) {
 
   napi_value async_name;
   NAPI_CALL(env, napi_create_string_utf8(env, "flock", NAPI_AUTO_LENGTH, &async_name));
-  NAPI_CALL(env, napi_async_init(env, args[2], async_name, &execute->async_context));
-  NAPI_CALL(env, napi_create_async_work(env, args[2], args[0], FlockExecute, FlockComplete, execute, &execute->async_work));
+  NAPI_CALL(env, napi_async_init(env, argv[2], async_name, &execute->async_context));
+  NAPI_CALL(env, napi_create_async_work(env, argv[2], argv[0], FlockExecute, FlockComplete, execute, &execute->async_work));
   NAPI_CALL(env, napi_queue_async_work(env, execute->async_work));
 
   napi_value ret;
@@ -88,8 +95,41 @@ static napi_value Flock (napi_env env, napi_callback_info cb_info) {
   return ret;
 }
 
+static napi_value StdErr (napi_env env, napi_callback_info cb_info) {
+  size_t argc = 1;
+  napi_value argv[argc];
+  NAPI_CALL(env, napi_get_cb_info(env, cb_info, &argc, argv, nullptr, nullptr));
+
+  int errno;
+  NAPI_CALL(env, napi_get_value_int32(env, argv[0], &errno));
+  char* err_desc = strerror(errno);
+
+  napi_value ret;
+  NAPI_CALL(env, napi_create_string_utf8(env, err_desc, NAPI_AUTO_LENGTH, &ret));
+  return ret;
+}
+
 NAPI_MODULE_INIT () {
   SET_NAMED_METHOD(env, exports, "flock", Flock);
+  SET_NAMED_METHOD(env, exports, "stdErr", StdErr);
+
+  napi_value lock_sh;
+  NAPI_CALL(env, napi_create_int32(env, LOCK_SH, &lock_sh));
+  napi_value lock_ex;
+  NAPI_CALL(env, napi_create_int32(env, LOCK_EX, &lock_ex));
+  napi_value lock_nb;
+  NAPI_CALL(env, napi_create_int32(env, LOCK_NB, &lock_nb));
+  napi_value lock_un;
+  NAPI_CALL(env, napi_create_int32(env, LOCK_UN, &lock_un));
+
+
+  napi_property_descriptor desc[] = {
+    { "LOCK_SH", 0, 0, 0, 0, lock_sh, napi_default, 0 },
+    { "LOCK_EX", 0, 0, 0, 0, lock_ex, napi_default, 0 },
+    { "LOCK_NB", 0, 0, 0, 0, lock_nb, napi_default, 0 },
+    { "LOCK_UN", 0, 0, 0, 0, lock_un, napi_default, 0 }
+  };
+  NAPI_CALL(env, napi_define_properties(env, exports, 4, desc));
 
   return exports;
 }
--- index.js
+++ index.js
@@ -1,1 +1,73 @@
-module.exports = require('./build/Release/flock.node')
+var fs = require('fs')
+var native = require('./build/Release/flock.node')
+
+function normalizeOptionsToMode (options) {
+  if (options == null) {
+    options = {}
+  }
+  if (typeof options !== 'object') {
+    throw new TypeError('Expect an object to be second argument of flock')
+  }
+  var mode = 0
+  if (!options.wait) {
+    mode = mode | native.LOCK_NB
+  }
+  if (options.exclusive) {
+    mode = mode | native.LOCK_EX
+  } else {
+    mode = mode | native.LOCK_SH
+  }
+  return mode
+}
+
+module.exports.lock = lock
+function lock (path, options, callback) {
+  if (typeof options === 'function') {
+    callback = options
+    options = null
+  }
+  var mode = normalizeOptionsToMode(options)
+  fs.open(path, 'w', (err, fd) => {
+    if (err) {
+      callback(err)
+      return
+    }
+    native.flock(fd, mode, ret => {
+      if (ret !== 0) {
+        var msg = native.stdErr(ret)
+        callback(new Error(msg))
+        return
+      }
+      callback(null, fd)
+    })
+  })
+}
+
+module.exports.upgrade = upgrade
+function upgrade (fd, options, callback) {
+  if (typeof options === 'function') {
+    callback = options
+    options = null
+  }
+  var mode = normalizeOptionsToMode(options)
+  native.flock(fd, mode, (ret) => {
+    if (ret !== 0) {
+      var msg = native.stdErr(ret)
+      callback(new Error(msg))
+      return
+    }
+    callback(null, fd)
+  })
+}
+
+module.exports.unlock = unlock
+function unlock (fd, callback) {
+  native.flock(fd, native.LOCK_UN, (ret) => {
+    if (ret !== 0) {
+      var msg = native.stdErr(ret)
+      callback(new Error(msg))
+      return
+    }
+    fs.close(fd, callback)
+  })
+}
